4fba3f45bb3e747f04e6430725cf6de4227cc39e
Himakshi35 <74656017+Himakshi35@users.noreply.github.com>
2021-04-25 17:09:09 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "AMAZING";
+const accessCode2 = "FAMOUS";
+const accessCode3 = "WONDERFUL";
+
+function clues() {
+    
+    fill("white");
+    textSize(30);
+    text("I A M Z N G A ", 160,75);
+    text("O F S A M U", 690,225);
+    text("D O F U N W R E L", 160,355);
+
+    
+    fill("lightblue");
+    textSize(25)
+    text("Hint: very surprising and difficult to believe !!", 70,100);
+    text("Hint: well known to many people !!", 590,250);
+    text("Hint: extremely good !!", 120,380);
+}
